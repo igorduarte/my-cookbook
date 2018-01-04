e791ea09db8173dd20473613140b2a331c6d5f09
Igor Duarte <igor.augustodr@gmail.com>
2018-01-04 18:56:58 -0200

    User registers recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,20 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+      :cook_time, :ingredients, :method)
+  end
 end
